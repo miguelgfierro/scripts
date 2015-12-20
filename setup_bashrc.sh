9ae263fe310b5e7ac76637c3156f1e13c6d9279a
hoaphumanoid <miguelgfierro@gmail.com>
2015-12-20 23:03:48 +0100

    setup bashrc

---
+++ setup_bashrc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set up several functions to .bashrc
+
+echo "
+################################################################################
+#Hide user name and host in terminal and make ls every time a terminal opens
+export PS1="\w$ "
+ls
+
+#cd + ls
+function cs () {
+    cd $1
+    ls
+}
+
+#transfer path: save the current path to a hidden file
+function tp () {
+    pwd > ~/.sp
+}
+
+#go to transfer path: goes where the previously saved tp points
+function gtp () {
+    cs `cat ~/.sp`
+}
+
+#cat with color
+function ccat () {
+    source-highlight -fesc -i $1
+}
+
+#Remove trash from terminal and runs program in background
+function evince () {
+    /usr/bin/evince $* 2> /dev/null & disown
+}
+function gedit (){
+	/usr/bin/gedit $* 2> /dev/null & disown
+}
+
+" >> ~/.bashrc
+
+echo .bashrc updated
