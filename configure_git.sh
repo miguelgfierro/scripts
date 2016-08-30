7d2465152fc6dfe08f80c57d99e240eb8a5688eb
hoaphumanoid <miguelgfierro@gmail.com>
2016-08-30 08:21:33 +0100

    configuring ssh access git

--- configure_git.sh
+++ configure_git.sh
@@ -2,18 +2,23 @@
 #
 # This script configure some global options in git like aliases, credential helper,
 # user name and email. To use:
-# $ sh configure.sh username email@example.com 
+# $ sh configure.sh 
 #
+echo "Configuring git"
+echo "Write your git username"
+read USER
+echo "Write your git email"
+read EMAIL
 
-USER=$1
-EMAIL=$2
-
+echo "Configuring global user name and email"
 git config --global user.name "$USER"
 git config --global user.email "$EMAIL"
 
+echo "Configuring global aliases"
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global credential.helper 'cache --timeout=36000'
 
-
+echo "Configuring git ssh access"
+ssh-keygen -t rsa -b 4096 -C "$EMAIL"
 
