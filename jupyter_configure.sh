fd41543795f82f6dc03e8df354241467b415f84a
hoaphumanoid <miguelgfierro@gmail.com>
2016-10-18 11:07:28 +0100

    add jupyter notebook config

---
+++ jupyter_configure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script configure jupyter notebook in an ubuntu server
+#
+
+if which jupyter-notebook >/dev/null; then
+    echo "Jupyter notebook exists"
+    ls -lha `which jupyter-notebook`
+else
+    echo "Jupyter notebook does not exist, please install"
+fi
