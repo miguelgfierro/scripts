5b9257f2544df2f4958607e6a6ec66841e74b6b3
hoaphumanoid <miguelgfierro@gmail.com>
2016-11-20 20:48:53 +0000

    updated setup bashrc with up and other improvements

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -15,10 +15,24 @@
 # We have to source the file instead of using sh. The reason is because the line
 # source ~./.bashrc will source the file in the sub-shell, i.e. a shell which 
 # is started as child process of the main shell.
+#
+# Contributors: Miguel Gonzalez-Fierro, Gustav Henning and Frederic Dauod
+
+# Exit if not sourced
+if [ "$0" == "$BASH_SOURCE" ]; then
+  echo >&2 "Setup script has to be sourced, not run with sh. Aborting"
+  exit 1
+fi
 
 # Create save path file if it is not created
 if [ ! -e ~/.sp ] ; then
     touch ~/.sp
+fi
+
+# To set up the bashrc script in Windows using cygwin it is necessary to add
+# at the beginning of the script to fix a problem with newlines the following:
+if [ $(uname -o) == "Cygwin" ]; then
+  (set -o igncr) 2>/dev/null && set -o igncr;
 fi
 
 echo "
@@ -58,8 +72,28 @@ function gedit (){
         /usr/bin/gedit \$* 2> /dev/null & disown
 }
 
+# up N: moves N times upwards (cd ../../../{N})
+function up () {
+  LIMIT=\$1
+  P=\$PWD
+  for ((i=1; i <= LIMIT; i++))
+  do
+      P=\$P/..
+  done
+  cs \$P
+  export MPWD=\$P
+}
+
 " >> ~/.bashrc
 
 source ~/.bashrc
+
+# if unix & dos2unix exists apply it
+if [ $(uname -o) == "Cygwin" ]; then
+  if [ command -v dos2unix >/dev/null 2>&1 ]; then
+    dos2unix $INTENDED_BASH_PATH
+  fi
+fi
+
 echo ".bashrc updated"
 
