754dcf5cd70a74e633ff7e1ed7006e60db15dc6c
Miguel Fierro <miguelgfierro@gmail.com>
2017-03-16 10:09:58 +0000

    updated mount fileshare with persistance in fstab

--- mount_azure_fileshare.sh
+++ mount_azure_fileshare.sh
@@ -31,3 +31,15 @@ fi
 
 sudo mkdir -p $MOUNT_POINT
 sudo mount -t cifs //$STORAGE_NAME.file.core.windows.net/$FILESHARE_NAME $MOUNT_POINT -o vers=3.0,username=$STORAGE_NAME,password=$STORAGE_KEY,dir_mode=0777,file_mode=0777
+
+read -r -p "Do you want to Persist the fileshare mount through reboots? [y/n] " RESP
+RESP=${RESP,,}    # tolower
+if [[ $RESP =~ ^(yes|y)$ ]]
+then
+    sudo echo "
+    ################################
+    #Line automatically added by scriptÑ mount_azure_fileshare.sh
+    //$STORAGE_NAME.file.core.windows.net/$FILESHARE_NAME $MOUNT_POINT cifs vers=3.0,username=$STORAGE_NAME,password=$STORAGE_KEY,dir_mode=0777,file_mode=0777
+    " >> /etc/fstab
+fi
+echo "Fileshare configured"
