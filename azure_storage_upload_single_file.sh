acf4a234a27de416a4f2d9878a447190e32d4f2c
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-11-27 15:48:36 +0000

    azure storage upload #29

--- azure_storage_upload_single_file.sh
+++ azure_storage_upload_single_file.sh
@@ -3,31 +3,32 @@
 # This bash script allows to upload a file to Azure Blob Storage.
 #
 # Usage:
-# sh azure_storage_upload_single_file.sh account_name account_key local_folder file_name destination_container
+# sh azure_storage_upload_single_file.sh account_name account_key local_filepath container_name remote_filepath
 # Example:
-# sh azure_storage_upload_single_file.sh azure_account1 3425324534eREWTSDER+2343243fsdfw4q3== /tmp test.csv tmp_container
+# sh azure_storage_upload_single_file.sh azure_account1 3425324534eREWTSDER+2343243fsdfw4q3== /tmp/test.csv tmp_container /data/temp.csv
 #
 # More info here: https://docs.microsoft.com/en-us/azure/storage/storage-azure-cli-nodejs
 #
 
 ACCOUNT_NAME=$1
 ACCOUNT_KEY=$2
-LOCAL_FOLDER=$3
-FILE_NAME=$4
-CONTAINER_NAME=$5
+LOCAL_FILEPATH=$3
+CONTAINER_NAME=$4
+REMOTE_FILEPATH=$5
 
 if [ "$#" -ne 5 ]; then
   echo ""
   echo "ERROR:Incorrect number of arguments"
   echo "Usage:"
-  echo "sh azure_storage_upload_single_file.sh account_name account_key local_folder file_name destination_container"
+  echo "sh azure_storage_upload_single_file.sh account_name account_key local_filepath container_name remote_filepath"
   exit 1
 fi
 
 export AZURE_STORAGE_ACCOUNT=$ACCOUNT_NAME
 export AZURE_STORAGE_ACCESS_KEY=$ACCOUNT_KEY
 
+azure telemetry --disable
 azure config mode arm
-azure storage blob upload $LOCAL_FOLDER/$FILE_NAME $CONTAINER_NAME $FILE_NAME
+azure storage blob upload $LOCAL_FILEPATH $CONTAINER_NAME $REMOTE_FILEPATH
 
 
