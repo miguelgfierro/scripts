c7270b2ab4e55f27cf7560ba8e146c8d38c77ba4
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-04-30 17:19:36 +0100

    azure blob upload batch check whether container exists

--- azure_blob_upload_folder_with_files.sh
+++ azure_blob_upload_folder_with_files.sh
@@ -16,7 +16,6 @@ LOCAL_FOLDER=$3
 CONTAINER_NAME=$4
 REMOTE_FOLDER=$5
 
-
 if [ "$#" -ne 5 ]; then
   echo ""
   echo "ERROR:Incorrect number of arguments"
@@ -28,5 +27,9 @@ fi
 export AZURE_STORAGE_ACCOUNT=$ACCOUNT_NAME
 export AZURE_STORAGE_KEY=$ACCOUNT_KEY
 
+# If the container does not exist, it creates one
+az storage container create -n $CONTAINER_NAME
+
+# Upload the data in batches
 az storage blob upload-batch --destination $CONTAINER_NAME/$REMOTE_FOLDER --source $LOCAL_FOLDER  
 
