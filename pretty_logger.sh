f7445950d47138c50cbda681b471eb67ba1e3aa9
miguelgfierro <miguelgfierro@users.noreply.github.com>
2025-03-25 22:49:45 +0100

    :memo:

--- pretty_logger.sh
+++ pretty_logger.sh
@@ -1,5 +1,12 @@
 # Script to display a log file with ERROR lines in red
 # It assumes that the errors are logged as for example: 
 # 2025-03-25 15:10:15 ERROR 'int' object has no attribute 'split'
+#
+# Usage:
+# sh pretty_logger.sh
+# sh pretty_logger.sh my_log_file.txt
 
-tail -n 200 -f app.log | awk '/ERROR/{print "\033[31m" $0 "\033[0m"; next} {print $0}'
+# Get the log file from command line argument or use default
+LOG_FILE="${1:-app.log}"
+
+tail -n 200 -f "$LOG_FILE" | awk '/ERROR/{print "\033[31m" $0 "\033[0m"; next} {print $0}'
